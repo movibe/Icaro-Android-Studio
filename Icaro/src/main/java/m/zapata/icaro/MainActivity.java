f93a22458bde5954b4190be9709f9a1b3ff0c10d
Miguel Angel Zapata <miguelo.zapata@gmail.com>
2013-08-14 15:23:40 -0400

    Implementados botones de ActionBar (Busqueda y microfono)

--- Icaro/src/main/java/m/zapata/icaro/MainActivity.java
+++ Icaro/src/main/java/m/zapata/icaro/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SearchView;
 
 public class MainActivity extends Activity {
 
@@ -19,6 +20,21 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_busqueda).getActionView();
+        //TODO: SetQueryHint con Android Resource
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+                //TODO: Implementar metodo para buscar
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
         return true;
     }
 
